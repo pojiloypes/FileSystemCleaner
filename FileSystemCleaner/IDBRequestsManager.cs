1a7dc4f742493466281427ba6c838020a2aa8123
Pojiloy_pes <83603088+pojiloypes@users.noreply.github.com>
2024-09-02 17:13:56 +0500

    Update IDBRequestsManager.cs

--- FileSystemCleaner/IDBRequestsManager.cs
+++ FileSystemCleaner/IDBRequestsManager.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace FileSystemCleaner
 {
+    // Интерфейс IDBRequestsManager определяет набор методов, которые должны быть реализованы для работы с запросами к базе данных.
     public interface IDBRequestsManager
     {
+        // Метод для получения SQL-запроса, который выбирает таблицы и столбцы, используемые в приложении.
         public string getSelectTablesAndColumnsRequest();
-        public string getSelectUnusedFilesRequest(List<Tuple<string, string>> tablesAndColumns);
+
+        // Метод для получения SQL-запроса, который выбирает неиспользуемые файлы на основе списка таблиц и столбцов.
+        public string getSelectUnusedFilesRequest(List<DBColumn> tablesAndColumns);
+
+        // Метод для получения SQL-запроса, который удаляет файлы из базы данных на основе списка файлов.
         public string getDeleteFilesRequest(List<File> files);
     }
 }
