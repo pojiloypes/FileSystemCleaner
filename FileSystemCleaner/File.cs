8492778600273442a6a4507192830c5af87739f3
Pojiloy_pes <83603088+pojiloypes@users.noreply.github.com>
2024-09-02 17:12:27 +0500

    Update File.cs

--- FileSystemCleaner/File.cs
+++ FileSystemCleaner/File.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace FileSystemCleaner
 {
+    // Класс File представляет файл, который хранится в файловой системе и связан с записью в базе данных.
     public class File
     {
+        // Свойство file_id хранит идентификатор файла в базе данных.
         public int file_id { get; set; }
+
+        // Свойство path хранит путь к файлу в файловой системе.
         public string path { get; set; }
 
+        // Конструктор инициализирует объект File, устанавливая его идентификатор и путь.
         public File(int file_id, string path)
         {
             this.file_id = file_id;
