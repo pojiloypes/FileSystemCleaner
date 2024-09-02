25473ad1a849c5c4ff6725c2cd48a1e65adcbe61
Pojiloy_pes <83603088+pojiloypes@users.noreply.github.com>
2024-09-02 17:15:57 +0500

    Update PostgreSqlRequestsManager.cs

--- FileSystemCleaner/PostgreSqlRequestsManager.cs
+++ FileSystemCleaner/PostgreSqlRequestsManager.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace FileSystemCleaner
 {
+    // Класс реализующий интерфейс IDBRequestsManager для работы с СУБД PostgreSQL
     internal class PostgreSqlRequestsManager : IDBRequestsManager
     {
         public string getSelectTablesAndColumnsRequest()
@@ -10,7 +11,7 @@ namespace FileSystemCleaner
             return "SELECT\r\n    tc.table_name AS referencing_table,\r\n    kcu.column_name AS referencing_column\r\nFROM\r\n    information_schema.table_constraints AS tc\r\nJOIN\r\n    information_schema.key_column_usage AS kcu\r\n    ON tc.constraint_name = kcu.constraint_name\r\n    AND tc.table_schema = kcu.table_schema\r\nJOIN\r\n    information_schema.constraint_column_usage AS ccu\r\n    ON ccu.constraint_name = tc.constraint_name\r\nWHERE\r\n    tc.constraint_type = 'FOREIGN KEY'\r\n    AND ccu.table_name = 'file'\r\n    AND ccu.column_name = 'file_id';";
         }
 
-        public string getSelectUnusedFilesRequest(List<Tuple<string, string>> tablesAndColumns)
+        public string getSelectUnusedFilesRequest(List<DBColumn> tablesAndColumns)
         {
             string res = "select file_id, path from file";
             if (tablesAndColumns.Count == 0)
@@ -19,7 +20,7 @@ namespace FileSystemCleaner
             res += " WHERE file_id NOT IN (";
             for (int i = 0; i < tablesAndColumns.Count; i++)
             {
-                res += "SELECT " + tablesAndColumns[i].Item2 + " FROM " + tablesAndColumns[i].Item1;
+                res += "SELECT " + tablesAndColumns[i].ColumnName + " FROM " + tablesAndColumns[i].TableName;
                 if (i < tablesAndColumns.Count - 1)
                     res += " UNION ";
                 else
