c96852addffac278118469d8ada2fe4e4966faf0
Pojiloy_pes <83603088+pojiloypes@users.noreply.github.com>
2024-09-02 17:12:00 +0500

    Add files via upload

---
+++ FileSystemCleaner/EmailSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace FileSystemCleaner
+{
+    // Класс EmailSettings хранит настройки для отправки электронной почты.
+    public class EmailSettings
+    {
+        // Свойство SenderEmail хранит адрес электронной почты отправителя.
+        public string SenderEmail { get; set; }
+
+        // Свойство SenderPassword хранит пароль для учетной записи отправителя.
+        public string SenderPassword { get; set; }
+
+        // Свойство RecipientEmail хранит адрес электронной почты получателя.
+        public string RecipientEmail { get; set; }
+
+        // Свойство SmtpServer хранит адрес SMTP-сервера для отправки почты.
+        public string SmtpServer { get; set; }
+
+        // Свойство SmtpPort хранит порт для подключения к SMTP-серверу.
+        public int SmtpPort { get; set; }
+    }
+}
